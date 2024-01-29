9948ce69e25a5fa8e54fe5d66a0abf3412b0a596
khang152 <121246182+Khang152@users.noreply.github.com>
2024-01-29 21:43:48 +0700

    [Account] Add Get Fee Rate API

---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/API/V5/Account/GetFeeRate.cs
@@ -0,0 +1,22 @@
+﻿using Bybit.Exchange.Net.Library;
+using Bybit.Exchange.Net.Models.Common;
+using Bybit.Exchange.Net.Models.V5.Account;
+
+namespace Bybit.Exchange.Net.API.V5
+{
+    public partial class BybitRestClient
+    {
+        public partial class Account
+        {
+            private string GetFeeRateUrl { get; set; } = "/v5/account/fee-rate";
+
+            public async Task<BybitResponse<GetFeeRateResponse>> GetFeeRateAsync(GetFeeRateRequest requestData)
+            {
+                var requestUrl = Utils.GetUrl(Options, GetFeeRateUrl);
+                var response = await Utils.GetData(Options, requestUrl, requestData);
+                var results = Utils.GetResponse<GetFeeRateResponse>(response);
+                return results;
+            }
+        }
+    }
+}
---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/Models/V5/Account/GetFeeRate.cs
@@ -0,0 +1,144 @@
+﻿using static Bybit.Exchange.Net.Data.Enums;
+
+namespace Bybit.Exchange.Net.Models.V5.Account
+{
+    public class GetFeeRateRequest
+    {
+        /// <summary>
+        /// <value>Property <c>category</c></value>
+        /// <remarks>
+        /// | Product type:
+        /// </remarks>
+        /// <list type="bullet">
+        /// <item>
+        /// <term>spot</term>
+        /// </item>
+        /// <item>
+        /// <term>linear</term>
+        /// </item>
+        /// <item>
+        /// <term>inverse</term>
+        /// </item>
+        /// <item>
+        /// <term>option</term>
+        /// </item>
+        /// </list>
+        /// </summary>
+        public Category? category { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>symbol</c></value>
+        /// <para>
+        /// Symbol name. Valid for linear, inverse, spot
+        /// </para>
+        /// </summary>
+        public string symbol { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>baseCoin</c></value>
+        /// <remarks>
+        /// | Base coin:
+        /// </remarks>
+        /// <list type="bullet">
+        /// <item>
+        /// <term>SOL</term>
+        /// </item>
+        /// <item>
+        /// <term>BTC</term>
+        /// </item>
+        /// <item>
+        /// <term>ETH</term>
+        /// </item>
+        /// </list>
+        /// <para>
+        /// Valid for option
+        /// </para>
+        /// </summary>
+        public string baseCoin { get; set; } = default!;
+    }
+
+    public class GetFeeRateResponse
+    {
+        /// <summary>
+        /// <value>Property <c>category</c></value>
+        /// <remarks>
+        /// | Product type:
+        /// </remarks>
+        /// <list type="bullet">
+        /// <item>
+        /// <term>spot</term>
+        /// </item>
+        /// <item>
+        /// <term>option</term>
+        /// </item>
+        /// </list>
+        /// <para>
+        /// Derivatives does not have this field
+        /// </para>
+        /// </summary>
+        public Category Category { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>list</c></value>
+        /// <para>
+        /// Object
+        /// </para>
+        /// </summary>
+        public List<GetFeeRateItem> List { get; set; } = default!;
+    }
+
+    /// <summary>
+    /// ListItem class for GetFeeRateResponse
+    /// </summary>
+    public class GetFeeRateItem
+    {
+        /// <summary>
+        /// <value>Property <c>symbol</c></value>
+        /// <para>
+        /// Symbol name. Keeps "" for Options
+        /// </para>
+        /// </summary>
+        public string Symbol { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>baseCoin</c></value>
+        /// <remarks>
+        /// | Base coin:
+        /// </remarks>
+        /// <list type="bullet">
+        /// <item>
+        /// <term>SOL</term>
+        /// </item>
+        /// <item>
+        /// <term>BTC</term>
+        /// </item>
+        /// <item>
+        /// <term>ETH</term>
+        /// </item>
+        /// </list>
+        /// <para>
+        /// Derivatives does not have this field
+        /// </para>
+        /// <para>
+        /// Keeps "" for Spot
+        /// </para>
+        /// </summary>
+        public string BaseCoin { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>takerFeeRate</c></value>
+        /// <para>
+        /// Taker fee rate
+        /// </para>
+        /// </summary>
+        public string TakerFeeRate { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>makerFeeRate</c></value>
+        /// <para>
+        /// Maker fee rate
+        /// </para>
+        /// </summary>
+        public string MakerFeeRate { get; set; } = default!;
+    }
+}
