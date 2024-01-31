ee81bbafbdeaf3f02e6611214dab4aa04a4e054c
khang152 <121246182+Khang152@users.noreply.github.com>
2024-01-31 23:42:52 +0700

    [C2C Lending] Add Redeem Funds API

---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/API/V5/C2CLending/RedeemFunds.cs
@@ -0,0 +1,22 @@
+﻿using Bybit.Exchange.Net.Library;
+using Bybit.Exchange.Net.Models.Common;
+using Bybit.Exchange.Net.Models.V5.C2CLending;
+
+namespace Bybit.Exchange.Net.API.V5
+{
+    public partial class BybitRestClient
+    {
+        public partial class C2CLending
+        {
+            private string RedeemFundsUrl { get; set; } = "/v5/lending/redeem";
+
+            public async Task<BybitResponse<RedeemFundsResponse>> RedeemFundsAsync(RedeemFundsRequest requestData)
+            {
+                var requestUrl = Utils.GetUrl(Options, RedeemFundsUrl);
+                var response = await Utils.PostData(Options, requestUrl, requestData);
+                var results = Utils.GetResponse<RedeemFundsResponse>(response);
+                return results;
+            }
+        }
+    }
+}
