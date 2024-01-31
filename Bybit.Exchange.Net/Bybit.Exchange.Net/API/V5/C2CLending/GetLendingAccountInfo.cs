90b243555f6edf26945eb77aecb4ab3e20b527d4
khang152 <121246182+Khang152@users.noreply.github.com>
2024-01-31 23:45:01 +0700

    [C2C Lending] Add Get Lending Account Info API

---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/API/V5/C2CLending/GetLendingAccountInfo.cs
@@ -0,0 +1,22 @@
+﻿using Bybit.Exchange.Net.Library;
+using Bybit.Exchange.Net.Models.Common;
+using Bybit.Exchange.Net.Models.V5.C2CLending;
+
+namespace Bybit.Exchange.Net.API.V5
+{
+    public partial class BybitRestClient
+    {
+        public partial class C2CLending
+        {
+            private string GetLendingAccountInfoUrl { get; set; } = "/v5/lending/account";
+
+            public async Task<BybitResponse<GetLendingAccountInfoResponse>> GetLendingAccountInfoAsync(GetLendingAccountInfoRequest requestData)
+            {
+                var requestUrl = Utils.GetUrl(Options, GetLendingAccountInfoUrl);
+                var response = await Utils.GetData(Options, requestUrl, requestData);
+                var results = Utils.GetResponse<GetLendingAccountInfoResponse>(response);
+                return results;
+            }
+        }
+    }
+}
---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/Models/V5/C2CLending/GetLendingAccountInfo.cs
@@ -0,0 +1,56 @@
+﻿namespace Bybit.Exchange.Net.Models.V5.C2CLending
+{
+    public class GetLendingAccountInfoRequest
+    {
+        /// <summary>
+        /// <value>Property <c>coin</c></value>
+        /// <para>
+        /// Coin name
+        /// </para>
+        /// </summary>
+        public string coin { get; set; } = default!;
+    }
+
+    public class GetLendingAccountInfoResponse
+    {
+        /// <summary>
+        /// <value>Property <c>coin</c></value>
+        /// <para>
+        /// Coin name
+        /// </para>
+        /// </summary>
+        public string Coin { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>principalInterest</c></value>
+        /// <para>
+        /// User Redeemable interest
+        /// </para>
+        /// </summary>
+        public string PrincipalInterest { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>principalQty</c></value>
+        /// <para>
+        /// Leftover quantity you can redeem for today (measured from 0 - 24 UTC), formula: min(the rest amount of principle, the amount that the user can redeem on the day)
+        /// </para>
+        /// </summary>
+        public string PrincipalQty { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>principalTotal</c></value>
+        /// <para>
+        /// Total amount redeemable by user
+        /// </para>
+        /// </summary>
+        public string PrincipalTotal { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>quantity</c></value>
+        /// <para>
+        /// Current deposit quantity
+        /// </para>
+        /// </summary>
+        public string Quantity { get; set; } = default!;
+    }
+}
