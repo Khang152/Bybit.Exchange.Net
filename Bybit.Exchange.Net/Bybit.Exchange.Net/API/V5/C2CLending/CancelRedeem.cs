44660188293bae1db5917ee937006ac8934257cd
khang152 <121246182+Khang152@users.noreply.github.com>
2024-01-31 23:43:33 +0700

    [C2C Lending] Add Cancel Redeem API

---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/API/V5/C2CLending/CancelRedeem.cs
@@ -0,0 +1,22 @@
+﻿using Bybit.Exchange.Net.Library;
+using Bybit.Exchange.Net.Models.Common;
+using Bybit.Exchange.Net.Models.V5.C2CLending;
+
+namespace Bybit.Exchange.Net.API.V5
+{
+    public partial class BybitRestClient
+    {
+        public partial class C2CLending
+        {
+            private string CancelRedeemUrl { get; set; } = "/v5/lending/redeem-cancel";
+
+            public async Task<BybitResponse<CancelRedeemResponse>> CancelRedeemAsync(CancelRedeemRequest requestData)
+            {
+                var requestUrl = Utils.GetUrl(Options, CancelRedeemUrl);
+                var response = await Utils.PostData(Options, requestUrl, requestData);
+                var results = Utils.GetResponse<CancelRedeemResponse>(response);
+                return results;
+            }
+        }
+    }
+}
---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/Models/V5/C2CLending/CancelRedeem.cs
@@ -0,0 +1,56 @@
+﻿namespace Bybit.Exchange.Net.Models.V5.C2CLending
+{
+    public class CancelRedeemRequest
+    {
+        /// <summary>
+        /// <value>Property <c>coin</c></value>
+        /// <para>
+        /// Coin name
+        /// </para>
+        /// </summary>
+        public string coin { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>orderId</c></value>
+        /// <para>
+        /// The order ID of redemption
+        /// </para>
+        /// </summary>
+        public string orderId { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>serialNo</c></value>
+        /// <para>
+        /// Serial no. The customised ID of redemption
+        /// </para>
+        /// </summary>
+        public string serialNo { get; set; } = default!;
+    }
+
+    public class CancelRedeemResponse
+    {
+        /// <summary>
+        /// <value>Property <c>orderId</c></value>
+        /// <para>
+        /// Order ID
+        /// </para>
+        /// </summary>
+        public string OrderId { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>serialNo</c></value>
+        /// <para>
+        /// Serial No
+        /// </para>
+        /// </summary>
+        public string SerialNo { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>updatedTime</c></value>
+        /// <para>
+        /// Updated timestamp (ms)
+        /// </para>
+        /// </summary>
+        public string UpdatedTime { get; set; } = default!;
+    }
+}
