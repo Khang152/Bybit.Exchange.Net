6ef517894a0402768aae27941016506f7ecf7637
khang152 <121246182+Khang152@users.noreply.github.com>
2024-02-03 12:58:42 +0700

    [Trade] Add Set DCP API

---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/API/V5/Trade/SetDcp.cs
@@ -0,0 +1,22 @@
+﻿using Bybit.Exchange.Net.Library;
+using Bybit.Exchange.Net.Models.Common;
+using Bybit.Exchange.Net.Models.V5.Trade;
+
+namespace Bybit.Exchange.Net.API.V5
+{
+    public partial class BybitRestClient
+    {
+        public partial class Trade
+        {
+            private string SetDcpUrl { get; set; } = "/v5/order/disconnected-cancel-all";
+
+            public async Task<BybitResponse<SetDcpResponse>> SetDcpAsync(SetDcpRequest requestData)
+            {
+                var requestUrl = Utils.GetUrl(Options, SetDcpUrl);
+                var response = await Utils.PostData(Options, requestUrl, requestData);
+                var results = Utils.GetResponse<SetDcpResponse>(response);
+                return results;
+            }
+        }
+    }
+}
---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/Models/V5/Trade/SetDcp.cs
@@ -0,0 +1,17 @@
+﻿namespace Bybit.Exchange.Net.Models.V5.Trade
+{
+    public class SetDcpRequest
+    {
+        /// <summary>
+        /// <value>Property <c>TimeWindow</c></value>
+        /// <para>
+        /// Disconnection timing window time. [3, 300], unit: second
+        /// </para>
+        /// </summary>
+        public int? timeWindow { get; set; } = default!;
+    }
+
+    public class SetDcpResponse
+    {
+    }
+}
