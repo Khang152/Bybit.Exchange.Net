ae90aa8baf66b8d909a1124bdc897c5fc0168675
khang152 <121246182+Khang152@users.noreply.github.com>
2024-02-03 12:49:51 +0700

    [Trade] Add Get Borrow Quota API

---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/API/V5/Trade/GetBorrowQuota.cs
@@ -0,0 +1,22 @@
+﻿using Bybit.Exchange.Net.Library;
+using Bybit.Exchange.Net.Models.Common;
+using Bybit.Exchange.Net.Models.V5.Trade;
+
+namespace Bybit.Exchange.Net.API.V5
+{
+    public partial class BybitRestClient
+    {
+        public partial class Trade
+        {
+            private string GetBorrowQuotaUrl { get; set; } = "/v5/order/spot-borrow-check";
+
+            public async Task<BybitResponse<GetBorrowQuotaResponse>> GetBorrowQuotaAsync(GetBorrowQuotaRequest requestData)
+            {
+                var requestUrl = Utils.GetUrl(Options, GetBorrowQuotaUrl);
+                var response = await Utils.GetData(Options, requestUrl, requestData);
+                var results = Utils.GetResponse<GetBorrowQuotaResponse>(response);
+                return results;
+            }
+        }
+    }
+}
---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/Models/V5/Trade/GetBorrowQuota.cs
@@ -0,0 +1,98 @@
+﻿using static Bybit.Exchange.Net.Data.Enums;
+
+namespace Bybit.Exchange.Net.Models.V5.Trade
+{
+    public class GetBorrowQuotaRequest
+    {
+        /// <summary>
+        /// <value>Property <c>category</c></value>
+        /// <para>
+        /// Product type: spot
+        /// </para>
+        /// </summary>
+        public Category? category { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>symbol</c></value>
+        /// <para>
+        /// Symbol name
+        /// </para>
+        /// </summary>
+        public string symbol { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>side</c></value>
+        /// <para>
+        /// Transaction side: Buy,Sell
+        /// </para>
+        /// </summary>
+        public Side? side { get; set; } = default!;
+    }
+
+    public class GetBorrowQuotaResponse
+    {
+        /// <summary>
+        /// <value>Property <c>symbol</c></value>
+        /// <para>
+        /// Symbol name
+        /// </para>
+        /// </summary>
+        public string Symbol { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>side</c></value>
+        /// <para>
+        /// Side
+        /// </para>
+        /// </summary>
+        public Side? side { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>maxTradeQty</c></value>
+        /// <remarks>
+        /// If spot margin trade on and symbol is margin trading pair, it returns available balance + max.borrowable amount.
+        /// Otherwise, it returns actual balance.
+        /// </remarks>
+        /// <para>
+        /// The maximum base coin qty can be traded, up to 4 decimals
+        /// </para>
+        /// </summary>
+        public string MaxTradeQty { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>maxTradeAmount</c></value>
+        /// <remarks>
+        /// If spot margin trade on and symbol is margin trading pair, it returns available balance + max.borrowable amount.
+        /// Otherwise, it returns actual balance.
+        /// </remarks>
+        /// <para>
+        /// The maximum quote coin amount can be traded, up to 8 decimals
+        /// </para>
+        /// </summary>
+        public string MaxTradeAmount { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>spotMaxTradeQty</c></value>
+        /// <remarks>
+        /// No matter your Spot margin switch on or not, it always returns actual qty of base coin you can trade or you have, up to 4 decimals
+        /// </remarks>
+        /// </summary>
+        public string SpotMaxTradeQty { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>spotMaxTradeAmount</c></value>
+        /// <remarks>
+        /// No matter your Spot margin switch on or not, it always returns actual amount of quote coin you can trade or you have, up to 8 decimals
+        /// </remarks>
+        /// </summary>
+        public string SpotMaxTradeAmount { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>borrowCoin</c></value>
+        /// <para>
+        /// Borrow coin
+        /// </para>
+        /// </summary>
+        public string BorrowCoin { get; set; } = default!;
+    }
+}
