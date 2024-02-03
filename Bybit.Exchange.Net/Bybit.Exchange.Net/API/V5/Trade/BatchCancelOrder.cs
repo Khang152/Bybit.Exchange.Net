80107b62d1d86bb0cd8abdc009642dfe172457fc
khang152 <121246182+Khang152@users.noreply.github.com>
2024-02-03 12:37:51 +0700

    [Trade] Add Batch Cancel Order API

---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/API/V5/Trade/BatchCancelOrder.cs
@@ -0,0 +1,22 @@
+﻿using Bybit.Exchange.Net.Library;
+using Bybit.Exchange.Net.Models.Common;
+using Bybit.Exchange.Net.Models.V5.Trade;
+
+namespace Bybit.Exchange.Net.API.V5
+{
+    public partial class BybitRestClient
+    {
+        public partial class Trade
+        {
+            private string BatchCancelOrderUrl { get; set; } = "/v5/order/cancel-batch";
+
+            public async Task<BybitBatchResponse<BatchCancelOrderResponse>> BatchCancelOrderAsync(BatchCancelOrderRequest requestData)
+            {
+                var requestUrl = Utils.GetUrl(Options, BatchCancelOrderUrl);
+                var response = await Utils.PostData(Options, requestUrl, requestData);
+                var results = Utils.GetBatchResponse<BatchCancelOrderResponse>(response);
+                return results;
+            }
+        }
+    }
+}
---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/Models/V5/Trade/BatchCancelOrder.cs
@@ -0,0 +1,69 @@
+﻿using static Bybit.Exchange.Net.Data.Enums;
+
+namespace Bybit.Exchange.Net.Models.V5.Trade
+{
+    public class BatchCancelOrderRequest
+    {
+        /// <summary>
+        /// <value>Property <c>Category</c></value>
+        /// <para>
+        /// Product type: linear, option | Covers: Option (UTA, UTA Pro) / USDT Perpetual, UDSC Perpetual, USDC Futures (UTA Pro)
+        /// </para>
+        /// </summary>
+        public Category? category { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>Request</c></value>
+        /// <para>
+        /// The Array Request
+        /// </para>
+        /// </summary>
+        public List<CancelOrderRequest>? request { get; set; } = default!;
+    }
+
+    public class BatchCancelOrderResponse
+    {
+        /// <summary>
+        /// <value>Property <c>List</c></value>
+        /// <para>
+        /// The Array Object
+        /// </para>
+        /// </summary>
+        public List<BatchCancelOrderItem>? List { get; set; } = default!;
+    }
+
+    public class BatchCancelOrderItem
+    {
+        /// <summary>
+        /// <value>Property <c>Category</c></value>
+        /// <para>
+        /// Product type: linear, option | Covers: Option (UTA, UTA Pro) / USDT Perpetual, UDSC Perpetual, USDC Futures (UTA Pro)
+        /// </para>
+        /// </summary>
+        public Category? Category { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>symbol</c></value>
+        /// <para>
+        /// Symbol name
+        /// </para>
+        /// </summary>
+        public string Symbol { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>orderId</c></value>
+        /// <para>
+        /// Order ID
+        /// </para>
+        /// </summary>
+        public string OrderId { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>orderLinkId</c></value>
+        /// <para>
+        /// User customised order ID
+        /// </para>
+        /// </summary>
+        public string OrderLinkId { get; set; } = default!;
+    }
+}
