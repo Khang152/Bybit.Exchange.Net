5ee89796488cfd4a9f4661b510c70156ae9ed90a
khang152 <121246182+Khang152@users.noreply.github.com>
2024-02-07 17:17:31 +0700

    [Broker] Add Get Exchange Broker Earning API

---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/API/V5/Broker/GetExchangeBrokerAccountInfo.cs
@@ -0,0 +1,22 @@
+﻿using Bybit.Exchange.Net.Library;
+using Bybit.Exchange.Net.Models.Common;
+using Bybit.Exchange.Net.Models.V5.Broker;
+
+namespace Bybit.Exchange.Net.API.V5
+{
+    public partial class BybitRestClient
+    {
+        public partial class Broker
+        {
+            private string GetExchangeBrokerAccountInfoUrl { get; set; } = "/v5/broker/account-info";
+
+            public async Task<BybitResponse<GetExchangeBrokerAccountInfoResponse>> GetExchangeBrokerAccountInfo(GetExchangeBrokerAccountInfoRequest requestData)
+            {
+                var requestUrl = Utils.GetUrl(Options, GetExchangeBrokerAccountInfoUrl);
+                var response = await Utils.GetData(Options, requestUrl, requestData);
+                var results = Utils.GetResponse<GetExchangeBrokerAccountInfoResponse>(response);
+                return results;
+            }
+        }
+    }
+}
---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/Models/V5/Broker/GetExchangeBrokerAccountInfo.cs
@@ -0,0 +1,68 @@
+﻿namespace Bybit.Exchange.Net.Models.V5.Broker
+{
+    public class GetExchangeBrokerAccountInfoRequest
+    {
+    }
+
+    public class GetExchangeBrokerAccountInfoResponse
+    {
+        /// <summary>
+        /// <value>Property <c>subAcctQty</c></value>
+        /// <para>
+        /// The qty of sub account has been created
+        /// </para>
+        /// </summary>
+        public string SubAcctQty { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>maxSubAcctQty</c></value>
+        /// <para>
+        /// The max limit of sub account can be created
+        /// </para>
+        /// </summary>
+        public string MaxSubAcctQty { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>baseFeeRebateRate</c></value>
+        /// <para>
+        /// Rebate percentage of the base fee
+        /// </para>
+        /// </summary>
+        public FeeRebateRateItem BaseFeeRebateRate { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>markupFeeRebateRate</c></value>
+        /// <para>
+        /// Rebate percentage of the mark up fee
+        /// </para>
+        /// </summary>
+        public FeeRebateRateItem MarkupFeeRebateRate { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>ts</c></value>
+        /// <para>
+        /// System timestamp (ms)
+        /// </para>
+        /// </summary>
+        public string Ts { get; set; } = default!;
+    }
+
+    public class FeeRebateRateItem
+    {
+        /// <summary>
+        /// <value>Property <c>spot</c></value>
+        /// <para>
+        /// Rebate percentage of the mark up fee for spot, e.g., 10.00%
+        /// </para>
+        /// </summary>
+        public string Spot { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>derivatives</c></value>
+        /// <para>
+        /// Rebate percentage of the mark up fee for derivatives, e.g., 10.00%
+        /// </para>
+        /// </summary>
+        public string Derivatives { get; set; } = default!;
+    }
+}
