10abd323cc037ba9b4c7f8a06a0107e163be5750
khang152 <121246182+Khang152@users.noreply.github.com>
2024-02-15 23:46:27 +0700

    [Spot Leverage Token] Add Redeem API

---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/API/V5/SpotLeverageToken/Redeem.cs
@@ -0,0 +1,22 @@
+﻿using Bybit.Exchange.Net.Library;
+using Bybit.Exchange.Net.Models.Common;
+using Bybit.Exchange.Net.Models.V5.SpotLeverageToken;
+
+namespace Bybit.Exchange.Net.API.V5
+{
+    public partial class BybitRestClient
+    {
+        public partial class SpotLeverageToken
+        {
+            private string PurchaseUrl { get; set; } = "/v5/spot-lever-token/purchase";
+
+            public async Task<BybitResponse<PurchaseResponse>> PurchaseAsync(PurchaseRequest requestData)
+            {
+                var requestUrl = Utils.GetUrl(Options, PurchaseUrl);
+                var response = await Utils.PostData(Options, requestUrl, requestData);
+                var results = Utils.GetResponse<PurchaseResponse>(response);
+                return results;
+            }
+        }
+    }
+}
---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/Models/V5/SpotLeverageToken/Redeem.cs
@@ -0,0 +1,96 @@
+﻿namespace Bybit.Exchange.Net.Models.V5.SpotLeverageToken
+{
+    public class RedeemRequest
+    {
+        /// <summary>
+        /// <value>Property <c>ltCoin</c></value>
+        /// <para>
+        /// Abbreviation of the LT, such as BTC3L
+        /// </para>
+        /// </summary>
+        public string ltCoin { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>quantity</c></value>
+        /// <para>
+        /// Redeem quantity of LT
+        /// </para>
+        /// </summary>
+        public string quantity { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>serialNo</c></value>
+        /// <para>
+        /// Serial number
+        /// </para>
+        /// </summary>
+        public string serialNo { get; set; } = default!;
+    }
+
+    public class RedeemResponse
+    {
+        /// <summary>
+        /// <value>Property <c>ltCoin</c></value>
+        /// <para>
+        /// Abbreviation of the LT, such as BTC3L
+        /// </para>
+        /// </summary>
+        public string LtCoin { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>ltOrderStatus</c></value>
+        /// <para>
+        /// Order status. 1: completed, 2: in progress, 3: failed
+        /// </para>
+        /// </summary>
+        public string LtOrderStatus { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>Quantity</c></value>
+        /// <para>
+        /// Redeem Quantity
+        /// </para>
+        /// </summary>
+        public string Quantity { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>execQty</c></value>
+        /// <para>
+        /// Executed qty of LT
+        /// </para>
+        /// </summary>
+        public string ExecQty { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>execAmt</c></value>
+        /// <para>
+        /// Executed amount of LT
+        /// </para>
+        /// </summary>
+        public string ExecAmt { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>RedeemId</c></value>
+        /// <para>
+        /// Order ID
+        /// </para>
+        /// </summary>
+        public string RedeemId { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>serialNo</c></value>
+        /// <para>
+        /// Serial number, customised order ID
+        /// </para>
+        /// </summary>
+        public string SerialNo { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>valueCoin</c></value>
+        /// <para>
+        /// Quote coin
+        /// </para>
+        /// </summary>
+        public string ValueCoin { get; set; } = default!;
+    }
+}
