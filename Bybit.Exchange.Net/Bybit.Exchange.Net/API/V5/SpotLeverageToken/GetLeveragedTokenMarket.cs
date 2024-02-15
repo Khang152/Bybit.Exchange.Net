ba9ce0f1edd375b0937faf5a83d4d25a5f86e2cc
khang152 <121246182+Khang152@users.noreply.github.com>
2024-02-15 23:45:35 +0700

    [Spot Leverage Token] Add Get Leveraged Token Market API

---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/API/V5/SpotLeverageToken/GetLeveragedTokenMarket.cs
@@ -0,0 +1,22 @@
+﻿using Bybit.Exchange.Net.Library;
+using Bybit.Exchange.Net.Models.Common;
+using Bybit.Exchange.Net.Models.V5.SpotLeverageToken;
+
+namespace Bybit.Exchange.Net.API.V5
+{
+    public partial class BybitRestClient
+    {
+        public partial class SpotLeverageToken
+        {
+            private string GetLeveragedTokenMarketUrl { get; set; } = "/v5/spot-lever-token/reference";
+
+            public async Task<BybitResponse<GetLeveragedTokenMarketResponse>> GetLeveragedTokenMarketAsync(GetLeveragedTokenMarketRequest requestData)
+            {
+                var requestUrl = Utils.GetUrl(Options, GetLeveragedTokenMarketUrl);
+                var response = await Utils.GetData(Options, requestUrl, requestData, useAPIKey: false);
+                var results = Utils.GetResponse<GetLeveragedTokenMarketResponse>(response);
+                return results;
+            }
+        }
+    }
+}
---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/Models/V5/SpotLeverageToken/GetLeveragedTokenMarket.cs
@@ -0,0 +1,64 @@
+﻿namespace Bybit.Exchange.Net.Models.V5.SpotLeverageToken
+{
+    public class GetLeveragedTokenMarketRequest
+    {
+        /// <summary>
+        /// <value>Property <c>ltCoin</c></value>
+        /// <para>
+        /// Abbreviation of the LT, such as BTC3L
+        /// </para>
+        /// </summary>
+        public string ltCoin { get; set; } = default!;
+    }
+
+    public class GetLeveragedTokenMarketResponse
+    {
+        /// <summary>
+        /// <value>Property <c>ltCoin</c></value>
+        /// <para>
+        /// Abbreviation of the LT, such as BTC3L
+        /// </para>
+        /// </summary>
+        public string LtCoin { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>nav</c></value>
+        /// <para>
+        /// Net value
+        /// </para>
+        /// </summary>
+        public string Nav { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>navTime</c></value>
+        /// <para>
+        /// Update time for net asset value (in milliseconds and UTC time zone)
+        /// </para>
+        /// </summary>
+        public string NavTime { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>circulation</c></value>
+        /// <para>
+        /// Circulating supply in the secondary market
+        /// </para>
+        /// </summary>
+        public string Circulation { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>basket</c></value>
+        /// <para>
+        /// Basket
+        /// </para>
+        /// </summary>
+        public string Basket { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>leverage</c></value>
+        /// <para>
+        /// Real leverage calculated by last traded price
+        /// </para>
+        /// </summary>
+        public string Leverage { get; set; } = default!;
+    }
+}
