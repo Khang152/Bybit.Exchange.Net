89fba6456f5f1454f777b62cc396aa38fabfb7ef
khang152 <121246182+Khang152@users.noreply.github.com>
2024-02-22 23:09:04 +0700

    [Spot Margin Trade (UTA)] Add Get Status And Leverage API

---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/API/V5/SpotMarginTradeUTA/GetStatusAndLeverage.cs
@@ -0,0 +1,25 @@
+﻿using Bybit.Exchange.Net.Library;
+using Bybit.Exchange.Net.Models.Common;
+using Bybit.Exchange.Net.Models.V5.SpotMarginTradeUTA;
+
+namespace Bybit.Exchange.Net.API
+{
+    public partial class V5
+    {
+        public partial class Endpoint
+        {
+            public partial class SpotMarginTradeUTA
+            {
+                private string GetStatusAndLeverageUrl { get; set; } = "/v5/spot-margin-trade/state";
+
+                public async Task<BybitResponse<GetStatusAndLeverageResponse>> GetStatusAndLeverageAsync()
+                {
+                    var requestUrl = Utils.GetUrl(Options, GetStatusAndLeverageUrl);
+                    var response = await Utils.GetData(Options, requestUrl, null);
+                    var results = Utils.GetResponse<GetStatusAndLeverageResponse>(response);
+                    return results;
+                }
+            }
+        }
+    }
+}
---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/Models/V5/SpotMarginTradeUTA/GetStatusAndLeverage.cs
@@ -0,0 +1,29 @@
+﻿namespace Bybit.Exchange.Net.Models.V5.SpotMarginTradeUTA
+{
+    public class GetStatusAndLeverageResponse
+    {
+        /// <summary>
+        /// <value>Property <c>spotLeverage</c></value>
+        /// <para>
+        /// Spot margin leverage. Returns "" if the margin trade is turned off
+        /// </para>
+        /// </summary>
+        public string SpotLeverage { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>spotMarginMode</c></value>
+        /// <para>
+        /// Spot margin status. 1: on, 0: off
+        /// </para>
+        /// </summary>
+        public string SpotMarginMode { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>effectiveLeverage</c></value>
+        /// <para>
+        /// Actual leverage ratio. Precision retains 2 decimal places, truncate downwards
+        /// </para>
+        /// </summary>
+        public string EffectiveLeverage { get; set; } = default!;
+    }
+}
