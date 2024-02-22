6bc3f595fcb24630a20f88e9d9d00cfe4f4a1f3b
khang152 <121246182+Khang152@users.noreply.github.com>
2024-02-22 23:08:35 +0700

    [Spot Margin Trade (UTA)] Add Set Leverage API

---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/API/V5/SpotMarginTradeUTA/SetLeverage.cs
@@ -0,0 +1,25 @@
+﻿using Bybit.Exchange.Net.Library;
+using Bybit.Exchange.Net.Models.Common;
+using Bybit.Exchange.Net.Models.V5.SpotMarginTradeUTA;
+
+namespace Bybit.Exchange.Net.API
+{
+    public partial class V5
+    {
+        public partial class Endpoint
+        {
+            public partial class SpotMarginTradeUTA
+            {
+                private string SetLeverageUrl { get; set; } = "/v5/spot-margin-trade/set-leverage";
+
+                public async Task<BybitResponse<SetLeverageResponse>> SetLeverageAsync(SetLeverageRequest requestData)
+                {
+                    var requestUrl = Utils.GetUrl(Options, SetLeverageUrl);
+                    var response = await Utils.PostData(Options, requestUrl, requestData);
+                    var results = Utils.GetResponse<SetLeverageResponse>(response);
+                    return results;
+                }
+            }
+        }
+    }
+}
---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/Models/V5/SpotMarginTradeUTA/SetLeverage.cs
@@ -0,0 +1,17 @@
+﻿namespace Bybit.Exchange.Net.Models.V5.SpotMarginTradeUTA
+{
+    public class SetLeverageRequest
+    {
+        /// <summary>
+        /// <value>Property <c>leverage</c></value>
+        /// <para>
+        /// Leverage. [2, 10]
+        /// </para>
+        /// </summary>
+        public string leverage { get; set; } = default!;
+    }
+
+    public class SetLeverageResponse
+    {
+    }
+}
