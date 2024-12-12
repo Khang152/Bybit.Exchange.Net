f78060c92a6bc777824d011c71733725f70aa44c
khang152 <khang152.tk@gmail.com>
2024-12-13 00:18:04 +0700

    [Position] Add Switch Position Mode API

---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/API/V5/Position/SwitchPositionMode.cs
@@ -0,0 +1,25 @@
+﻿using Bybit.Exchange.Net.Library;
+using Bybit.Exchange.Net.Models.Common;
+using Bybit.Exchange.Net.Models.V5.Position;
+
+namespace Bybit.Exchange.Net.API
+{
+    public partial class V5
+    {
+        public partial class Endpoint
+        {
+            public partial class Position
+            {
+                private string SwitchPositionModeUrl { get; set; } = "/v5/position/switch-mode";
+
+                public async Task<BybitResponse<SwitchPositionModeResponse>> SwitchPositionModeAsync(SwitchPositionModeRequest requestData)
+                {
+                    var requestUrl = Utils.GetUrl(Options, SwitchPositionModeUrl);
+                    var response = await Utils.PostData(Options, requestUrl, requestData);
+                    var results = Utils.GetResponse<SwitchPositionModeResponse>(response);
+                    return results;
+                }
+            }
+        }
+    }
+}
---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/Models/V5/Position/SwitchPositionMode.cs
@@ -0,0 +1,50 @@
+﻿using static Bybit.Exchange.Net.Data.Enums;
+
+namespace Bybit.Exchange.Net.Models.V5.Position
+{
+    public class SwitchPositionModeRequest
+    {
+        /// <summary>
+        /// Product type
+        /// <remarks>
+        /// <para>
+        /// UTA2.0: linear, USDT Perp
+        /// </para>
+        /// <para>
+        /// UTA1.0: linear, USDT Perp; inverse, Inverse Futures
+        /// </para>
+        /// <para>
+        /// Classic: linear, USDT Perp; inverse, Inverse Futures
+        /// </para>
+        /// </remarks>
+        /// </summary>
+        public required Category category { get; set; } = default!;
+
+        /// <summary>
+        /// Symbol name, like BTCUSDT, uppercase only. Either symbol or coin is required. Symbol has a higher priority
+        /// </summary>
+        public string? symbol { get; set; }
+
+        /// <summary>
+        /// Coin, uppercase only
+        /// </summary>
+        public string? coin { get; set; }
+
+        /// <summary>
+        /// Position mode
+        /// <remarks>
+        /// <para>
+        /// 0: Merged Single
+        /// </para>
+        /// <para>
+        /// 3: Both Sides
+        /// </para>
+        /// </remarks>
+        /// </summary>
+        public required int mode { get; set; }
+    }
+
+    public class SwitchPositionModeResponse
+    {
+    }
+}
