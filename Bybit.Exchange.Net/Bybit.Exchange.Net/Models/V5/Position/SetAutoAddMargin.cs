4d48494658493bc0d42f099ee32199b3a6faebdf
khang152 <khang152.tk@gmail.com>
2025-09-28 00:29:35 +0700

    [Position] Set Auto Add Margin API

---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/Models/V5/Position/SetAutoAddMargin.cs
@@ -0,0 +1,37 @@
+﻿using static Bybit.Exchange.Net.Data.Enums;
+
+namespace Bybit.Exchange.Net.Models.V5.Position
+{
+    public class SetAutoAddMarginRequest
+    {
+        /// <summary>
+        /// Product type.
+        /// <para>UTA2.0, UTA1.0: linear (USDT Contract, USDC Contract)</para>
+        /// <para>Classic account: linear (USDT Perps)</para>
+        /// </summary>
+        public Category category { get; set; }
+
+        /// <summary>
+        /// Symbol name, like BTCUSDT, uppercase only.
+        /// </summary>
+        public string symbol { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Turn on/off auto add margin.
+        /// <para>0 = off, 1 = on</para>
+        /// </summary>
+        public int autoAddMargin { get; set; }
+
+        /// <summary>
+        /// Used to identify positions in different position modes. Required for hedge mode positions.
+        /// <para>0 = one-way mode</para>
+        /// <para>1 = hedge-mode Buy side</para>
+        /// <para>2 = hedge-mode Sell side</para>
+        /// </summary>
+        public int? positionIdx { get; set; }
+    }
+
+    public class SetAutoAddMarginResponse
+    {
+    }
+}
