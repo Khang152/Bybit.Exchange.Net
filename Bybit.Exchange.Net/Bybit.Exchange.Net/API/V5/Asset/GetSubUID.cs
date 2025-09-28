b6693513dcba38f927a5fb6870d31d520e150098
khang152 <khang152.tk@gmail.com>
2025-09-28 19:11:17 +0700

    [Asset] Get Sub UID API

---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/API/V5/Asset/GetSubUID.cs
@@ -0,0 +1,25 @@
+﻿using Bybit.Exchange.Net.Library;
+using Bybit.Exchange.Net.Models.Common;
+using Bybit.Exchange.Net.Models.V5.Asset;
+
+namespace Bybit.Exchange.Net.API
+{
+    public partial class V5
+    {
+        public partial class Endpoint
+        {
+            public partial class Asset
+            {
+                private string GetSubUIDUrl { get; set; } = "/v5/asset/transfer/query-sub-member-list";
+
+                public async Task<BybitResponse<GetSubUIDResponse>> GetSubUIDAsync(GetSubUIDRequest requestData)
+                {
+                    var requestUrl = Utils.GetUrl(Options, GetSubUIDUrl);
+                    var response = await Utils.GetData(Options, requestUrl, requestData);
+                    var results = Utils.GetResponse<GetSubUIDResponse>(response);
+                    return results;
+                }
+            }
+        }
+    }
+}
---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/Models/V5/Asset/GetSubUID.cs
@@ -0,0 +1,22 @@
+﻿using static Bybit.Exchange.Net.Data.Enums;
+
+namespace Bybit.Exchange.Net.Models.V5.Asset
+{
+    public class GetSubUIDRequest
+    {
+
+    }
+
+    public class GetSubUIDResponse
+    {
+        /// <summary>
+        /// All sub UIDs under the main UID.
+        /// </summary>
+        public List<string> subMemberIds { get; set; } = new();
+
+        /// <summary>
+        /// All sub UIDs that have universal transfer enabled.
+        /// </summary>
+        public List<string> transferableSubMemberIds { get; set; } = new();
+    }
+}
